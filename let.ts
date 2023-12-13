41cf1361361fb8b1512977bd55fdfc7a03e537a0
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-13 15:06:06 +0530

    Let statement Block Statements

--- let.ts
+++ let.ts
@@ -11,3 +11,18 @@ function letblock(name: string) {
 }
 letblock("taranjeet");
 letblock("random");
+
+// One More Example For Block Statement
+
+var a = 10;
+var b = 20;
+if (a == 10) {
+  let a = 30;
+  var b = 40;
+  console.log(a);
+  console.log(b);
+}
+console.log(a);
+console.log(b);
+
+// Output : 30 40 10 40
