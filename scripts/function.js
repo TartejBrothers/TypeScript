5dc8617051af75143209da8167254f921f23a14f
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-13 12:46:20 +0530

    Function

---
+++ scripts/function.js
@@ -0,0 +1,14 @@
+"use strict";
+function say(name) {
+    if (name === "taranjeet") {
+        console.log("hello taranjeet");
+    }
+    else if (name === "sachin") {
+        console.log("hello sachin");
+    }
+    else {
+        console.log("hello guest");
+    }
+}
+say("taranjeet");
+say("random");
