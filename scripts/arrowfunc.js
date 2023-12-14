b7f4ec9f709f20f5bebcaebeff9bc28f4956ae8f
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-14 13:05:13 +0530

    Arrow Function

--- scripts/arrowfunc.js
+++ scripts/arrowfunc.js
@@ -1,15 +1,16 @@
+"use strict";
 // Old Way
 var getVal = function () {
-  return 10;
+    return 10;
 };
 // Arrow Function using Return
 var getValArrow = () => {
-  return 10;
+    return 10;
 };
 // Arrow Function without Return
 var getValArrowReturn = () => 10;
 // Arrow Function with Parameters
-var getValArrowPara = (m: any, n: any) => m + n;
+var getValArrowPara = (m, n) => m + n;
 console.log(getVal());
 console.log(getValArrow());
 console.log(getValArrowReturn());
---
+++ scripts/letloops.js
@@ -0,0 +1,14 @@
+"use strict";
+for (var i = 1; i <= 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+// Output: 6 6 6 6 6
+// Using Let will give the expected output
+for (let i = 1; i <= 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+// Output: 1 2 3 4 5
