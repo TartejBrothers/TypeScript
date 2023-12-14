8804155f7b6785d812bf6aa62084b87dceff1f1c
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-14 12:31:15 +0530

    Const Keywords

---
+++ const.ts
@@ -0,0 +1,5 @@
+const namenew = "Hello";
+// const namenew = "World"; This will lead to an Error
+// It is used to create read only keywords and cannot be changed.
+// They are not hoisted just like var.
+// A value must be assigned to it while declaration and that value cannot be changed.
