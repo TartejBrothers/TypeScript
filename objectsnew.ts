8f25b2a91efd0b1cb5e32303881493d201ba1072
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-15 18:26:23 +0530

    Object Literals New

---
+++ objectsnew.ts
@@ -0,0 +1,11 @@
+let ln = "last name";
+let personnew = {
+  "first name": "Taranjeet",
+  [ln]: "Singh",
+};
+console.log(personnew["first name"] + " " + personnew[ln]);
+
+// OR
+console.log(personnew["first name"] + " " + personnew["last name"]);
+
+
