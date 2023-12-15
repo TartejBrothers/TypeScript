01a0b0b53e3b814439a0e150f4b9924641d598f8
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-15 18:31:26 +0530

    Destruct Arrays

---
+++ destructarray.ts
@@ -0,0 +1,11 @@
+let employees = ["taranjeet", "singh", "bedi"];
+let [fname, mname, lname] = employees;
+console.log(fname);
+console.log(mname);
+console.log(lname);
+
+//output : 
+// taranjeet
+// singh
+// bedi
+
