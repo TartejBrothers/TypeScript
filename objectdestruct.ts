722b420e76962bb5358b4d3f0582b22b29984475
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-17 00:28:50 +0530

    Destruct Objects Alias

--- objectdestruct.ts
+++ objectdestruct.ts
@@ -8,3 +8,14 @@ let { newname, age, place } = object;
 console.log(newname);
 console.log(age);
 console.log(place);
+
+let objectnew = {
+  objnewname: "sai",
+  objage: 21,
+  objplace: "hyd",
+};
+let { objnewname: nname, objage: nage, objplace: nplace } = objectnew;
+
+console.log(nname);
+console.log(nage);
+console.log(nplace);
