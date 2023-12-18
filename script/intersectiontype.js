6ee8f86734a1775470728d25f3d756fd9a0b3b7a
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-18 13:16:58 +0530

    intersection

--- script/intersectiontype.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// We can create our own types that are a combination of other types
-let oldweight = {
-    weight: () => console.log("weight"),
-    newweight: () => console.log("newweight"),
-};
